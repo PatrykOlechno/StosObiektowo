021ce1a1df53bf73646f7ee96d0d838de8027515
Patryk Olechno <patrykoolechno@gmail.com>
2021-03-16 13:53:59 +0100

    Added Pop Push Display

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace StosStrukturalnie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stos1 = new Stos(5);
+            var stos2 = new Stos(5);
+
+            stos1.Push(1);
+            stos1.Push(2);
+            stos1.Push(3);
+            stos1.Push(4);
+            stos1.Pop();
+            stos1.Pop();
+            stos1.Display();
+
+            stos2.Push(3);
+            stos2.Push(2);
+            stos2.Push(4);
+            stos2.Pop();
+            stos2.Pop();
+            stos2.Display();
+            stos1.Display();
+            Console.WriteLine(stos1.Top());
+            Console.WriteLine(stos2.Top());
+        }
+    }
+}
---
+++ Stos.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StosStrukturalnie
+{
+    class Stos
+    {
+        public int Count;
+        private int[] stack;
+
+        public Stos(int size)
+        {
+            stack = new int[size];
+            Count = 0;
+        }
+
+        public void Push(int number)
+        {
+            if (Count >= stack.Length)
+            {
+                Console.WriteLine("Stack is full");
+            }
+            else
+            {
+                stack[Count] = number;
+                Count++;
+            }
+            
+        }
+
+        public int Pop()
+        {
+            if(Count == 0)
+            {
+                Console.WriteLine("Stack is empty");
+                return -1;
+            }
+            else
+            {
+                Count--;
+                return stack[Count];
+            }
+            
+        }
+
+        public void Display()
+        {
+            if (Count == 0) {
+                Console.WriteLine("Stos jest pusty");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Co na stosie: ");
+                for(int i = Count - 1; i>= 0; i--)
+                {
+                    Console.WriteLine(stack[i]);
+                }
+            }
+        }
+
+        public int Top()
+        {
+            return stack[Count];
+        }
+
+    }
+}
